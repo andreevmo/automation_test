f776974be49861464b49882e4fc050d5e07bae66
andreevmo <maxiandreev1994@gmail.com>
2023-09-02 17:14:14 +0400

    Добавил степы в BaseTest и статический импорт в SelenideConfig

--- src/main/java/ru/andreev/utils/SelenideConfig.java
+++ src/main/java/ru/andreev/utils/SelenideConfig.java
@@ -1,28 +1,27 @@
 package ru.andreev.utils;
 
-import com.codeborne.selenide.Configuration;
-
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.codeborne.selenide.Configuration.*;
 import static java.lang.System.getenv;
 
 public abstract class SelenideConfig {
 
     public static void setup() {
         if (getenv("IS_SELENOID") != null) {
-            Configuration.remote = "http://localhost:4444/wd/hub";
+            remote = "http://localhost:4444/wd/hub";
             Map<String, Boolean> options = new HashMap<>();
             options.put("enableVNC", true);
             options.put("enableVideo", Boolean.valueOf(getenv("IS_VIDEO")));
             options.put("enableLog", true);
-            Configuration.browserCapabilities.setCapability("selenoid:options", options);
+            browserCapabilities.setCapability("selenoid:options", options);
         }
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 10 * 1000;
-        Configuration.pageLoadTimeout = 10 * 1000;
-        Configuration.downloadsFolder = "src/test/resources/downloads";
-        Configuration.fastSetValue = true;
+        browser = "chrome";
+        browserSize = "1920x1080";
+        timeout = 10 * 1000;
+        pageLoadTimeout = 10 * 1000;
+        downloadsFolder = "src/test/resources/downloads";
+        fastSetValue = true;
     }
 }
--- src/test/java/ru/andreev/BaseTest.java
+++ src/test/java/ru/andreev/BaseTest.java
@@ -1,5 +1,6 @@
 package ru.andreev;
 
+import io.qameta.allure.Step;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -12,11 +13,13 @@ import static com.codeborne.selenide.WebDriverRunner.hasWebDriverStarted;
 @Listeners(CustomListener.class)
 public class BaseTest {
 
+    @Step("Настроить selenide")
     @BeforeMethod
     public void beforeMethod() {
         SelenideConfig.setup();
     }
 
+    @Step("Закрыть драйвер")
     @AfterMethod
     public void afterMethod() {
         if (hasWebDriverStarted()) {
